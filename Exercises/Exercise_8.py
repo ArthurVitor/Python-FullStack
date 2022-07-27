bdd790960c9eae8a832a06adeda28936214b0135
Arthur <arthurvitorgn@gmail.com>
2022-07-27 17:46:38 -0300

    Exercise

--- Exercises/Exercise_8.py
+++ Exercises/Exercise_8.py
@@ -1,12 +1,13 @@
-from itertools import zip_longest, count
+# lista_A = [1,2,3,4,5,6,7,8]
+# lista_B = [1,2,3,4]
+# lista_C = zip(lista_A, lista_B)
+# tot = []
+# for c in lista_C:
+#     tot.append(sum(c))
+#
+# print(tot) Minha solução
 
-indice = count()
-cidades = ['São Paulo', 'Belo Horizonte', 'Salvador', 'Monte Belo']
-estados = ['SP', 'MG', 'BA']
-cidades_estados = zip(
-    indice,
-    cidades,
-    estados)
-
-for valor in cidades_estados:
-    print(valor )
+lista_A = [1,2,3,4,5,6,7,8]
+lista_B = [1,2,3,4]
+tot = [x + y for x, y in zip(lista_A, lista_B)]
+print(tot)
