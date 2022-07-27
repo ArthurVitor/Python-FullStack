61d2685e947af3ce71ad4d40edfa8942377a07ac
Arthur <arthurvitorgn@gmail.com>
2022-07-27 15:54:33 -0300

    Exercise

--- Exercises/Exercise_6.py
+++ Exercises/Exercise_6.py
@@ -1,8 +1,21 @@
-string = '01234567890123456789012345678901234567890123456789'
-lista = []
-min, sep = 0, 10
-for c in range(0, int(string.count('9'))):
-    lista.append(string[min:sep])
-    min, sep + 10, 10
+# string = '012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789'
+#
+# def stringSplitting(string):
+#     lista = []
+#     stt, sep = 0, 10
+#     for c in range(0, int(string.count('9'))):
+#         lista.append(string[stt:sep])
+#         stt, sep + 10, 10
+#     return lista
+#
+#
+# if __name__ == '__main__':
+#     print(stringSplitting(string))
+# Minha solução
 
-print(lista, sep='_')
+# Solução do video
+string = '012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789'
+n = 10
+lista = [(string[i: i + n]) for i in range(0, len(string), n)]
+retorno = '.'.join(lista)
+print(retorno)
