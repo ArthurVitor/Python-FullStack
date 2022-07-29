66f4645495f30954af88356aa7971e5daa75a963
Arthur <arthurvitorgn@gmail.com>
2022-07-29 18:07:45 -0300

    Map

---
+++ Classes/Dados.py
@@ -0,0 +1,27 @@
+produto = [
+    {'nome': 'p1', 'preco': 13},
+    {'nome': 'p2', 'preco': 42},
+    {'nome': 'p3', 'preco': 14},
+    {'nome': 'p4', 'preco': 32},
+    {'nome': 'p5', 'preco': 321},
+    {'nome': 'p6', 'preco': 51.9},
+    {'nome': 'p7', 'preco': 42},
+    {'nome': 'p8', 'preco': 60},
+    {'nome': 'p9', 'preco': 49},
+    {'nome': 'p10', 'preco': 37}
+]
+
+pessoas = [
+    {'nome': 'Clebin', 'idade': 4},
+    {'nome': 'Tutu', 'idade': 6},
+    {'nome': 'Shurastei', 'idade': 12},
+    {'nome': 'Shuraigo', 'idade': 67},
+    {'nome': 'Vini', 'idade': 33},
+    {'nome': 'Alfred', 'idade': 6},
+    {'nome': 'Daniel', 'idade': 32},
+    {'nome': 'James', 'idade':51},
+    {'nome': 'Tue', 'idade': 43},
+    {'nome': 'Arthur', 'idade': 15},
+]
+
+lista = [1,2,3,4,5,6,7,8,9,10]
