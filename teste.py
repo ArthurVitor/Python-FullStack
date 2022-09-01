05886c518fc6957e2edcd00d20485e8564b3ce21
Arthur <arthurvitorgn@gmail.com>
2022-08-31 21:24:20 -0300

    Public, protected and private

--- teste.py
+++ teste.py
@@ -1,5 +1,11 @@
-lista = [1,2,3,4,5,6]
-teste = []
-teste.append(lista.pop())
-print(lista)
-print(teste)
+alunos = list([
+    {'id': 15, 'nome': 'Arthur'},
+    {'id': 51, 'nome': 'Cleber'},
+    {'id': 412, 'nome': 'Pedro'},
+    {'id': -15, 'nome': 'Tutu'}
+])
+
+alunos.append({'id': 84, 'nome': 'Astolfo'})
+alunos.sort(key=lambda item: item['id'])
+for c in alunos:
+    print(c)
