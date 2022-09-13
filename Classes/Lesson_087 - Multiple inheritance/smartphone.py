2a0a89aaae86a77802797cdef9c7df608eb6346b
Arthur <arthurvitorgn@gmail.com>
2022-09-12 21:02:52 -0300

    Multiple inheritance

--- Classes/Lesson_087 - Multiple inheritance/smartphone.py
+++ Classes/Lesson_087 - Multiple inheritance/smartphone.py
@@ -1,25 +1,38 @@
 from eletronico import Eletronico
+from log import LogMixin
 
 
-class Smartphone(Eletronico):
+class Smartphone(Eletronico, LogMixin):
     def __init__(self, nome):
         super().__init__(nome)
         self._conectado = False
 
     def conectar(self):
         if not self._ligado:
-            print(f'{self._nome} não está ligado')
+            info = f'{self._nome} não está ligado'
+            print(info)
+            self.log_info(f'{self._nome} não está ligado')
             return
 
         if self._conectado:
-            print(f'{self._nome} já está conectado')
+            error = f'{self._nome} já está conectado'
+            print(error)
+            self.error(error)
             return
 
         self._conectado = True
-        print(f'{self._nome} está conectado')
+        info = f'{self._nome} está conectado'
+        print(info)
+        self.log_info(info)
 
     def desconectar(self):
         if not self._conectado:
-            print(f'{self._nome} não está conectado')
+            error = f'{self._nome} não está conectado'
+            print(error)
+            self.error(error)
             return
+
+        info = f'{self._nome} foi desligado com sucesso'
+        print(info)
+        self.log_info(info)
         self._conectado = False
